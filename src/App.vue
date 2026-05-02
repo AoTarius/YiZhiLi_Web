4fed19ecc47150d79b804e573da569e6a79747de
AoTarius <1836411990@qq.com>
2026-05-02 09:04:50 +0800

    初始化提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import MainPage from './components/MainPage.vue'
+</script>
+
+<template>
+  <MainPage />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f5f5f0; /* 素雅背景色 */
+  min-height: 100vh;
+}
+
+#app {
+  background-color: #f5f5f0;
+}
+</style>
